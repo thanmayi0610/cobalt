6a32ff71f82effa137f2597d4b819db984f2d163
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-15 10:55:54 +0530

    update Open Graph image URL for improved social sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
     description:
       "Explore the fascinating world of cobalt. From its role in clean energy to cutting-edge technology, Cobalt is your guide to this powerful metal.",
     images: {
-      url: "/og-card.png",
+      url: "cobalt-ruby-nu.vercel.app/og-card.png",
       width: 1200,
       height: 628,
     },
