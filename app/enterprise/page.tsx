43064ad083d4b351a1e8d2b6f88f79b1bf669938
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-15 10:53:29 +0530

    remove unused React import from Enterprise component and add new Pricing page component

--- app/enterprise/page.tsx
+++ app/enterprise/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 const Enterprise = () => {
     return <h1>Enterprise</h1>;
 }
---
+++ app/pricing/page.tsx
@@ -0,0 +1,4 @@
+const Page = () => {
+    return <h1>Enterprise</h1>;
+}
+export default Page;
