32325c8ab9408353cd60ab819e5dae7089a799e7
thanmayi0610 <thanmayithanmayi81@gmail.com>
2025-04-15 13:43:38 +0530

    refactor(blog): replace static blog list with dynamic mapping for improved scalability

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,14 +1,20 @@
+import Link from "next/link";
 import {PropsWithChildren} from "react";
 export default ( props: PropsWithChildren) => {
     return(
     <div className = "h-screen flex flex-row ">
         <div className = "w-48">
             
-            <ul className="flex flex-col  ">
-                <li>Blog 1</li>
-                <li>Blog 2</li>
-                <li>Blog 3</li>
-            </ul>
+        <ul className="flex flex-col list-none">
+  {[1, 2, 3, 4].map((slug) => {
+    return (
+      <li key={slug}>
+        <Link href={`/blog/${slug}`}>Blog {slug}</Link>
+      </li>
+    );
+  })}
+</ul>
+
         </div>
         <div className = "w-full">{props.children}</div>
 
